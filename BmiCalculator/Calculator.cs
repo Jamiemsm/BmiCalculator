73d327509afdd02dcdd2aad8d584d0466ccddf63
Kasper Kraack <MrKraack@gmail.com>
2019-02-04 11:05:03 +0100

    Calculate added

---
+++ BmiCalculator/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BmiCalculator
+{
+    class Calculator
+    {
+        public int Heigth;
+        public int Weigth;
+        public int bmi;
+
+        public void Calulate()
+        {
+            bmi = (Weigth / (Heigth * Heigth)) * 703;
+            
+        }
+
+
+
+    }
+}
